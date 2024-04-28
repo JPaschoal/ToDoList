52f963ceb08e962bbbd328c29adb4dddf60e9b4f
Jhonatan Paschoal <jhonatan.paschoal@performait.com>
2024-04-28 12:29:41 -0300

    Adding annotations and fix contructor

--- ToDoList.Domain/Models/ToDoItem.cs
+++ ToDoList.Domain/Models/ToDoItem.cs
@@ -1,9 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ToDoList.Domain.Models;
 
 public class ToDoItem
 {
+    public ToDoItem()
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.Now;
+    }
+    
+    [Key]
+    [Required]
     public Guid Id { get; set; }
-    public string Title { get; set; }
+    
+    [Required(ErrorMessage = "Title is required")]
+    public string Title { get; set; } = string.Empty;
+    
     public bool IsDone { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? CompletedAt { get; set; }
--- ToDoList.Domain/Models/ToDoList.cs
+++ ToDoList.Domain/Models/ToDoList.cs
@@ -1,8 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ToDoList.Domain.Models;
 
 public class ToDoList
 {  
-public Guid Id { get; set; }
+    public ToDoList()
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.Now;
+    }
+    [Key]
+    public Guid Id { get; set; }
+    [Required(ErrorMessage = "Title is required")]
     public string Title { get; set; }
+    public DateTime CreatedAt { get; set; }
     public virtual ICollection<ToDoItem> Items { get; set; }
 }
