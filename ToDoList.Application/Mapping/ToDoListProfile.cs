4eca2cc39aed76b1008e4efa4df04f5a63644080
Jhonatan Paschoal <jhonatan.paschoal@performait.com>
2024-04-29 20:56:06 -0300

    Refactor mapping and service methods in ToDoListProfile.cs and ToDoService.cs

--- ToDoList.Application/Mapping/ToDoListProfile.cs
+++ ToDoList.Application/Mapping/ToDoListProfile.cs
@@ -3,23 +3,14 @@ using ToDoList.Domain.Dtos.Request;
 using ToDoList.Domain.Dtos.Response;
 using ToDoList.Domain.Models;
 
-namespace ToDoList.Data.Mapping;
+namespace ToDoList.Application.Mapping;
 
 public class ToDoListProfile : Profile
 {
     public ToDoListProfile()
     {
-        CreateMap<CreateToDoListRequest, ListToDo>()
-            .ForMember(dest => dest.Title, 
-                opt => opt.MapFrom(x => x.Title));
-        CreateMap<ListToDo, ToDoListResponse>()
-            .ForMember(dest => dest.Id,
-                opt => opt.MapFrom(x => x.Id))
-            .ForMember(dest => dest.Title,
-                opt => opt.MapFrom(x => x.Title))
-            .ForMember(dest => dest.CreatedAt,
-                opt => opt.MapFrom(x => x.CreatedAt))
-            .ForMember(dest => dest.UpdatedAt,
-                opt => opt.MapFrom(x => x.UpdatedAt));
+        CreateMap<CreateToDoListRequest, ListToDo>();
+        CreateMap<UpdateToDoListRequest, ListToDo>();
+        CreateMap<ListToDo, ToDoListResponse>();
     }
 }
